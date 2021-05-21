0776849cf647e5e6543e108666689a384320247d
Andy <andykohl3r@gmail.com>
2021-05-21 09:09:51 +0200

    Save commit

--- src/components/site.js
+++ src/components/site.js
@@ -11,6 +11,17 @@ export default {
             users: [],
             personal: false,
             listdata: [],
+            sfxData: [],
+            sfxAutoComplete: [],
+            recipientSearchQuery: '',
+            searchResultOrg: [],
+            searchResultTeam: [],
+            searchResult: [],
+            searchResultUsers: [],
+            userOrgList: [],
+            userTeamList: [],
+            exchangeRecipient: '',
+            tags: [],
         };
     },
 
@@ -27,6 +38,18 @@ export default {
             this.organisations = data.organisations;
             this.teams = data.teams;
 
+            this.sfxData.push(
+                this.organisations
+                    .map(org => ({name: org.name, key: `o_${org.id}`, value: `o_${org.id}`})
+                )
+            )
+
+            this.sfxData.push(
+                this.teams
+                    .map(team => ({name: team.name, key: `t_${team.id}`, value: `t_${team.id}`, connector: team.orgId})
+                )
+            )
+
             this.organisations.forEach(org => { 
                 this.treeOrgData.push({title:org.name, value: `o_${org.id}`, key: `o_${org.id}`, children: 
                     this.teams
@@ -35,23 +58,79 @@ export default {
                 });
             });
 
+            // Create User List
             this.organisations.forEach(org => {
-                org.users.forEach(user => {
-                    this.users.push({title: user.email, value: user.email, key: user.email})
+                org.users.forEach(user => { 
+                    this.addUser(user);
                 });
             });
-
-            console.log(this.users);
-            console.log(this.treeOrgData);
+            // Add orgs to a user
+            this.organisations.forEach(org => {
+                this.users.forEach(user => {
+                    if(org.users.filter(orgUser => orgUser.email === user.email).length > 0){
+                        user.orgs.push({id: org.id, name: org.name, key: org.id, value: org.id, teams: 
+                            org.teams.map(team => this.getTeamByID(team))
+                        });
+                    }
+                });
+            });
+            // Add teams to a user
+            this.teams.forEach(team => {
+                this.users.forEach(user => {
+                    if(team.users.filter(teamUser => teamUser.email === user.email).length > 0){
+                        user.teams.push({id: team.id, name: team.name, key: team.id, value: team.id});
+                    }
+                });
+            });
         },
-
-        inputperson(){
-            if (this.inputRecipient !== undefined){
-                this.personal = false;
-            } else {
-                this.personal = true;
+        getTeamByID(id){
+            let team = (this.teams.filter(team => team.id === id));
+            return team[0].name;
+        },
+        addUser(user){
+            let userObject = {
+                email: user.email,
+                key: user.email,
+                value: user.email,
+                orgs: [],
+                teams: []
+            }
+            if(!this.users.filter(user => user.email === userObject.email).length > 0){
+                this.users.push(userObject);
             }
         },
+        handleRecipientSearch(query){
+            if(query) {
+                switch(query.charAt(0)) {
+                    case '@':
+                        this.getQueryEntities(query);
+                        break;
+                    default:
+                        this.getQueryUsers(query);
+                        break;
+                }
+            }
+        },
+        handleRecipientSelect(recipient){
+            this.tags.push(recipient);
+            this.exchangeRecipient = '';
+            this.searchResult = [];
+        },
+        getQueryEntities(query){
+            this.searchResultOrg = (this.sfxData[0].filter(entry => entry.name.toLowerCase().substring(0, (query.length-1)) === query.toLowerCase().substring(1, query.length)).map(entry => entry));
+            this.searchResultTeam = (this.sfxData[1].filter(entry => entry.name.toLowerCase().substring(0, (query.length-1)) === query.toLowerCase().substring(1, query.length)).map(entry => entry));
+
+            this.searchResult.push(this.searchResultOrg).concat(this.searchResultTeam);
+        },
+        getQueryUsers(query){
+            if(this.users.filter(user => user.email === query).length > 0){
+                this.searchResult = this.users.filter(entry => entry.email.toLowerCase().substring(0, query.length) === query.toLowerCase().substring(0, query.length))
+            } else {
+                this.searchResult = [];
+            }
+
+        },
+        
         handleRecipientType(type){
             const selected = type.toString();
             let selectedType = selected.split('_')[0]
@@ -82,20 +161,9 @@ export default {
                 });
             }
         },
-        addOrg() {
-            if (this.inputPersonal == undefined) {
-                this.listdata.push(...this.users)
-
-            }
-            this.inputPersonal = this.inputPersonal.filter((user) => !this.listdata.includes(user));
-            this.listdata.push(...this.inputPersonal)
-            this.inputPersonal = undefined;
-            console.log(this.listdata);
-        },
-
-        deleteuser(user) {
-            var index = this.listdata.indexOf(user);
-            this.listdata.splice(index, 1);
+        handleClose(removedTag) {
+            const tags = this.tags.filter(tag => tag !== removedTag);
+            this.tags = tags;
         },
     },
 }
--- src/components/site.vue
+++ src/components/site.vue
@@ -23,44 +23,82 @@
             &nbsp;&nbsp;&nbsp;&nbsp;
             <a-icon type="swap" />
             &nbsp;&nbsp;&nbsp;&nbsp;
-            <a-tree-select
-                v-model="inputRecipient"
-                show-search
-                :style="{ width: '25rem', maxWidth: '100%' }"
-                :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
-                placeholder="Please select"
-                allow-clear
-                multiple
-                :treeData="treeOrgData"
-                @blur="inputRecipient = undefined"
-                @change="handleRecipientType"
-             >
-            </a-tree-select>
-            </div>
-            <div v-infinite-scroll="handleInfiniteOnLoad"
-                class="demo-infinite-container"
-                :infinite-scroll-disabled="busy"
-                :infinite-scroll-distance="10">
-                <a-list item-layout="horizontal" :data-source="listdata">
-                    <a-list-item class="list" v-for="user in listdata" :key="user">
-                        {{user}}
-                        <a class="deletebutton" @click="deleteuser(user)">delete</a>
-                    </a-list-item>
-                </a-list>
-            </div>
-            <div class="inputfield_inputcontainer1" v-if="this.inputRecipient !== undefined">
-                <a-tree-select
-                    v-model="inputPersonal"
-                    show-search
-                    :style="{ width: '25rem', maxWidth: '100%' }"
-                    :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
-                    placeholder="Please select"
-                    allow-clear
-                    multiple
-                    :treeData="users"
+
+        
+
+
+            <a-auto-complete
+                v-model="exchangeRecipient"
+                class="certain-category-search"
+                dropdown-class-name="certain-category-search-dropdown"
+                :dropdown-match-select-width="false"
+                :dropdown-style="{ width: '300px' }"
+                size="large"
+                :style="{ width: '25rem'}"
+                placeholder="input here"
+                option-label-prop="value"
+                @select="handleRecipientSelect"
+                @change="handleRecipientSearch"
                 >
-                </a-tree-select>
-                <a-button class="addbutton" @click="addOrg()" type="primary">Add</a-button>
+                
+                <template slot="dataSource">
+
+                    <template v-for="entry in searchResult" :value="entry.name">
+                        {{entry.email}}
+                        <a-select-opt-group :value="entry.name" :key="entry.key">
+                            <span slot="label">
+                                User
+                            </span>
+
+                            <a-select-option :value="entry.name" :key="entry.key">
+                                <a-icon type="user" />
+                                {{ entry.email }}
+                            </a-select-option>                        
+                        </a-select-opt-group>
+
+                        <a-select-opt-group v-for="entryOrg in entry.orgs" :value="entryOrg.name" :key="entryOrg.key">
+                            <span slot="label">
+                                {{ entryOrg.name }}
+                            </span>
+
+                            <a-select-option :value="entryOrg.name" :key="entryOrg.key">
+                                <a-icon type="bank" />
+                                {{ entryOrg.name }}
+                            </a-select-option>    
+
+                            <a-select-option v-for="entryTeam in entryOrg.teams" :value="entryTeam" :key="entryTeam">
+                                <a-icon type="apartment" />
+                                {{ entryTeam }}
+                            </a-select-option> 
+
+                        </a-select-opt-group>
+
+
+
+                    </template>
+
+                </template>
+                
+                <a-input>
+                    <a-icon slot="suffix" type="search" class="certain-category-icon" />
+                </a-input>
+            </a-auto-complete>
+
+            <template>
+            <div :style="{ marginLeft: '27.2rem', width: '25rem'}">
+                <template v-for="(tag) in tags">
+                <a-tooltip v-if="tag.length > 20" :key="tag" :title="tag">
+                    <a-tag :key="tag" :closable="true" @close="() => handleClose(tag)">
+                    {{ `${tag.slice(0, 20)}...` }}
+                    </a-tag>
+                </a-tooltip>
+                <a-tag v-else :key="tag" :closable="true" @close="() => handleClose(tag)">
+                    {{ tag }}
+                </a-tag>
+                </template>
+            </div>
+            </template>
+            <br>
             </div>
         </div>   
     </div>
